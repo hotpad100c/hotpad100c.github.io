a060a80a0b1ee0260221052661f237648870f7db
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-11-05 10:44:34 +0900

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,10 +1,14 @@
-import { defaultTheme } from 'vuepress'
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
 
-export default {
+export default defineUserConfig({
   lang: 'zh-CN',
-  title: '我的博客',
-  description: '这是一个使用 VuePress 搭建的静态博客',
+  title: 'Ryan100c的博客',
+  description: '这是Ryan100c的VuePress博客！',
   theme: defaultTheme({
-    navbar: [{ text: '首页', link: '/' }],
+    navbar: [
+      { text: '首页', link: '/' },
+      { text: '关于', link: '/about/' },
+    ],
   }),
-}
+})
