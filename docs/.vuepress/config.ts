5c24895a20047e7ebfe1c4c0572915cad8d36e2b
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-11-05 10:59:30 +0900

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,14 +1,35 @@
-import { defineUserConfig } from 'vuepress'
-import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from "vuepress";
+import theme from "vuepress-theme-plume";
 
 export default defineUserConfig({
-  lang: 'zh-CN',
-  title: 'Ryan100c的博客',
-  description: '这是Ryan100c的VuePress博客！',
-  theme: defaultTheme({
+  lang: "zh-CN",
+  title: "Ryan100c的博客",
+  description: "-。",
+
+  theme: theme({
+    logo: "/logo.png",
+    favicon: "/logo.png",
+
     navbar: [
-      { text: '首页', link: '/' },
-      { text: '关于', link: '/about/' },
+      { text: "首页", link: "/" },
+      { text: "文章", link: "/posts/" },
+      { text: "关于", link: "/about/" },
+      { text: "GitHub", link: "https://github.com/hotpad100c" },
     ],
+
+    social: [
+      { icon: "github", link: "https://github.com/hotpad100c" },
+    ],
+
+    blog: {
+      avatar: "/logo.png",
+      name: "Ryan100c",
+      description: "-",
+    },
+
+    footer: {
+      message: "使用 VuePress + Plume 主题构建, 参考 https://github.com/ZXBHELLO/Website",
+      copyright: "Copyright © 2025 Ryan100c",
+    },
   }),
-})
+});
