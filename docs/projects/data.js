f42142007b2e9a27ef0e0924d05c3585965b7b6a
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-11-10 11:03:32 +0900

    Add navigation data structure to data.js for test

---
+++ docs/projects/data.js
@@ -0,0 +1,50 @@
+export const navData = [
+  {
+    category: '模组',
+    items: [
+      {
+        id: 'A',
+        title: '模组1',
+        description: '介绍1',
+        url: '//',
+        iconType: 'iconify',
+        icon: 'material-symbols:article-outline',
+        showIconBorder: true,
+      },
+      
+      {
+        id: 'B',
+        title: '2',
+        description: 'e1',
+        url: '//',
+        iconType: 'iconify',
+        icon: 'material-symbols:article-outline',
+        showIconBorder: true,
+      }
+    ]
+  },
+  {
+    category: '工具',
+    items: [
+      {
+        id: 'A',
+        title: '模',
+        description: '介绍1',
+        url: '//',
+        iconType: 'iconify',
+        icon: 'material-symbols:article-outline',
+        showIconBorder: true,
+      },
+      
+      {
+        id: 'B',
+        title: '2',
+        description: 'e1',
+        url: '//',
+        iconType: 'iconify',
+        icon: 'material-symbols:article-outline',
+        showIconBorder: true,
+      }
+    ]
+  }
+]
