6fc127310f9560b39b1c35c636cf74fb53080d94
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-11-10 10:57:57 +0900

    Add NavPage component registration
    Register NavPage component in VuePress client config.

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,3 +1,9 @@
 import { defineClientConfig } from 'vuepress/client'
 
 import './styles/index.css'
+import NavPage from './components/NavPage.vue'
+export default defineClientConfig({
+  enhance({ app }) {
+    app.component('NavPage', NavPage) // 添加这一行来注册 NavPage 组件
+  },
+})
